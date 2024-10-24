9e923f97ce9509dca83424e32a163956b4502bd3
Matthew Clark <matt.clark@ves.solutions>
2024-10-24 14:21:37 -0400

    Add new files

---
+++ recent_pictures_retrieval/app/app.py
@@ -0,0 +1,13 @@
+import os
+import time
+import pika
+
+credentials = pika.PlainCredentials(os.environ.get('RABBITMQ_USER', 'myuser'), os.environ.get('RABBITMQ_PASS', 'mypassword'))
+parameters = pika.ConnectionParameters(host=os.environ.get('RABBITMQ_HOST', 'localhost'), credentials=credentials)
+connection = pika.BlockingConnection(parameters)
+
+
+while True:
+    time.sleep(10)
+
+
